b98477846e834504a2b5ee9dcaef114cbfcbabfe
Dabo Ross <dabodialga@gmail.com>
2013-08-04 01:09:15 -0700

    Created

--- src/main/java/net/daboross/bukkitdev/permslow/PermSlowPlugin.java
+++ src/main/java/net/daboross/bukkitdev/permslow/PermSlowPlugin.java
@@ -17,12 +17,24 @@
 package net.daboross.bukkitdev.permslow;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.mcstats.MetricsLite;
 
 /**
@@ -30,6 +42,8 @@ import org.mcstats.MetricsLite;
  * @author daboross
  */
 public class PermSlowPlugin extends JavaPlugin implements Listener {
+
+    private final Map<String, BukkitTask> map = new HashMap<String, BukkitTask>();
 
     @Override
     public void onEnable() {
@@ -52,10 +66,38 @@ public class PermSlowPlugin extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("")) {
-        } else {
-            sender.sendMessage("PermSlow doesn't know about the command /" + cmd.getName());
-        }
+        sender.sendMessage("PermSlow doesn't know about the command /" + cmd.getName());
         return true;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent evt) {
+        runIt(evt.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent evt) {
+        runIt(evt.getPlayer().getName());
+    }
+
+    private void runIt(final String username) {
+        BukkitTask lastTask = map.get(username.toLowerCase());
+        if (lastTask != null) {
+            lastTask.cancel();
+        }
+        int duration = 20 * (int) TimeUnit.MINUTES.toSeconds(20);
+        final PotionEffect effect = new PotionEffect(PotionEffectType.SLOW, duration, 3);
+        BukkitRunnable runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Player p = Bukkit.getPlayer(username);
+                if (p != null) {
+                    p.addPotionEffect(effect, true);
+                } else {
+                    this.cancel();
+                }
+            }
+        };
+        map.put(username.toLowerCase(), runnable.runTaskTimer(this, 0, duration));
     }
 }
